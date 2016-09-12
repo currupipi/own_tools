3218fafe7a3c351366b1f4c77efe3adc061b1ba4
mishkin <pisterrassa@gmail.com>
2016-09-13 00:46:15 +0200

    Adding more stuff to the draft for sqli scanner

--- sqli_scanner/sqli_scanner.py
+++ sqli_scanner/sqli_scanner.py
@@ -4,13 +4,27 @@
 import requests
 import sys
 import argparse
+import random
+import pdb
 
-def sqli_test(fullurl):
-	try:
-		result = requests.get(fullurl)
-	except Exception as msg:
-		print '[!] ERROR: {} Can not GET from url {}'.format(msg,url)
-		sys.exit(1)
+def sqli_test(url, payload=None):
+	"""payload needs to be a well formed dict"""
+	result = -1
+	user_agent_list = ['Mozilla/5.0 (Windows NT 6.3; rv:48.0) Gecko/20100101 Firefox/48.0', 'Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko']	
+	user_agent = user_agent_list[random.randint(0,1)]
+	headers = {'user_agent': user_agent }
+	if payload is None:
+		try:
+			result = requests.get(url, headers=headers)
+		except Exception as msg:
+			print '[!] ERROR: {} Can not GET from url {}'.format(msg,url)
+			pass
+	else:
+		try:
+			result = requests.get(url,headers=headers, params=payload)
+		except Exception as msg:
+			print '[!] ERROR: {} Can not GET from url {} with payload {}'.format(msg,url,payload)
+			pass
 	return result
 
 # Console colors
@@ -23,7 +37,8 @@ B = '\033[34m' # blue
 help_message = ("USAGE:\n" +
                 "The sqli_scanner.py checks sqli injection\n" +
 				"Example:\n" +
-				"sqli_scanner.py -t url.com") 
+				"sqli_scanner.py -t url.com\n" +
+				"sqli_scanner.py -t url.com -s some_sqli_params") 
 parser = argparse.ArgumentParser(usage=help_message, description='Tests sqli injection')
 parser.add_argument('-t', help='URL of the target host', default=False)
 parser.add_argument('-s', help='sqli test', default=False, action='store_true')
@@ -33,24 +48,26 @@ url = args.t
 sqli = args.s
 
 #SQLI tests
-sqli_list = [ "or 1 = 1"]
+sqli_list = [ "1 order by 1", "1 or 1 = 1", "jerry or 1 =1"]
 
 if __name__ == "__main__":
 	
 	print G + "[+] INFO: Starting check" + W
 
+#	pdb.set_trace()
+
 	if sqli:
-		fullurl = url + sqli
-		print fullurl
-		print G + "[+] INFO: Trying {} on {}".format(sqli, url) + W
+		fullurl = url +  sqli
+		print G + "[+] INFO: Trying {}".format(fullurl) + W
 		result = sqli_test(fullurl)
-		print G + "[+] INFO: Output from is {} ".format(result) + W
+		print G + "[+] INFO: Output is: " +W
+		print result.text
 	
 	else:
 		for sqli in sqli_list:
 			fullurl = url + sqli
-			print fullurl
-			print G + "[+] INFO: Trying {} on {}".format(sqli, url) + W
+			print G + "[+] INFO: Trying {}".format(fullurl) + W
 			result = sqli_test(fullurl)
-			print G + "[+] INFO: Output from is {} ".format(result) + W
+			print G + "[+] INFO: Output is:" +W
+			print result.text
 
